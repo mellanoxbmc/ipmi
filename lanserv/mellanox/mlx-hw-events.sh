40dbf9f0e05a81f0ed7b900a8a63d3922fdde4b4
Nataliya Yakuts <nataliyay@mellanox.com>
2017-02-08 16:49:58 +0200

    Updated symlinks creation (-f option)
    Signed-off-by: Nataliya Yakuts <nataliyay@mellanox.com>
    Reviewed-by: Vadim Pasternak <vadimp@mellanox.com>

--- lanserv/mellanox/mlx-hw-events.sh
+++ lanserv/mellanox/mlx-hw-events.sh
@@ -3,22 +3,22 @@
 if [ "$1" == "add" ]; then
   if [ "$2" == "amb_carrier" ] || [ "$2" == "amb_switch" ]; then
     mkdir -p /bsp/thermal/
-    ln -s $3$4/temp1_input /bsp/thermal/$2_temp
-    ln -s $3$4/temp1_max /bsp/thermal/$2_temp_max
-    ln -s $3$4/temp1_max_hyst /bsp/thermal/$2_temp_hyst
+    ln -sf $3$4/temp1_input /bsp/thermal/$2_temp
+    ln -sf $3$4/temp1_max /bsp/thermal/$2_temp_max
+    ln -sf $3$4/temp1_max_hyst /bsp/thermal/$2_temp_hyst
   fi
   if [ "$2" == "psu1" ] || [ "$2" == "psu2" ]; then
     mkdir -p /bsp/thermal/
     mkdir -p /bsp/environment/
-    ln -s $3$4/temp1_input /bsp/thermal/$2_temp
-    ln -s $3$4/temp1_max /bsp/thermal/$2_temp_max
-    ln -s $3$4/temp1_max_alarm /bsp/thermal/$2_temp_alarm
-    ln -s $3$4/in1_input /bsp/environment/$2_vin
-    ln -s $3$4/in2_input /bsp/environment/$2_vout
-    ln -s $3$4/power1_input /bsp/environment/$2_pin
-    ln -s $3$4/power2_input /bsp/environment/$2_pout
-    ln -s $3$4/curr1_input /bsp/environment/$2_iin
-    ln -s $3$4/curr2_input /bsp/environment/$2_iout
+    ln -sf $3$4/temp1_input /bsp/thermal/$2_temp
+    ln -sf $3$4/temp1_max /bsp/thermal/$2_temp_max
+    ln -sf $3$4/temp1_max_alarm /bsp/thermal/$2_temp_alarm
+    ln -sf $3$4/in1_input /bsp/environment/$2_vin
+    ln -sf $3$4/in2_input /bsp/environment/$2_vout
+    ln -sf $3$4/power1_input /bsp/environment/$2_pin
+    ln -sf $3$4/power2_input /bsp/environment/$2_pout
+    ln -sf $3$4/curr1_input /bsp/environment/$2_iin
+    ln -sf $3$4/curr2_input /bsp/environment/$2_iout
 
     #FAN spped set
     if [ "$2" == "psu1" ]; then
@@ -29,144 +29,144 @@ if [ "$1" == "add" ]; then
   fi
   if [ "$2" == "A2D" ]; then
     mkdir -p /bsp/environment/
-    ln -s $3$4/in_voltage_voltage_scale /bsp/environment/$2_voltage_scale
-    ln -s $3$4/in_voltage6_raw /bsp/environment/$2_18v
-    ln -s $3$4/in_voltage5_raw /bsp/environment/$2_12v
-    ln -s $3$4/in_voltage4_raw /bsp/environment/$2_vcore
-    ln -s $3$4/in_voltage3_raw /bsp/environment/swb_$2_12v
-    ln -s $3$4/in_voltage2_raw /bsp/environment/swb_$2_3aux
-    ln -s $3$4/in_voltage1_raw /bsp/environment/swb_$2_3sen
+    ln -sf $3$4/in_voltage_voltage_scale /bsp/environment/$2_voltage_scale
+    ln -sf $3$4/in_voltage6_raw /bsp/environment/$2_18v
+    ln -sf $3$4/in_voltage5_raw /bsp/environment/$2_12v
+    ln -sf $3$4/in_voltage4_raw /bsp/environment/$2_vcore
+    ln -sf $3$4/in_voltage3_raw /bsp/environment/swb_$2_12v
+    ln -sf $3$4/in_voltage2_raw /bsp/environment/swb_$2_3aux
+    ln -sf $3$4/in_voltage1_raw /bsp/environment/swb_$2_3sen
   fi
   if [ "$2" == "ADC" ]; then
     mkdir -p /bsp/environment/
-    ln -s $3$4/adc0_value /bsp/environment/$2_12v
-    ln -s $3$4/adc3_value /bsp/environment/$2_5v
-    ln -s $3$4/adc4_value /bsp/environment/$2_5vusb
-    ln -s $3$4/adc10_value /bsp/environment/$2_3vaux
-    ln -s $3$4/adc11_value /bsp/environment/$2_3vbmc
-    ln -s $3$4/adc12_value /bsp/environment/$2_2vddr
-    ln -s $3$4/adc13_value /bsp/environment/$2_1ddr
-    ln -s $3$4/adc14_value /bsp/environment/$2_1vcore
+    ln -sf $3$4/adc0_value /bsp/environment/$2_12v
+    ln -sf $3$4/adc3_value /bsp/environment/$2_5v
+    ln -sf $3$4/adc4_value /bsp/environment/$2_5vusb
+    ln -sf $3$4/adc10_value /bsp/environment/$2_3vaux
+    ln -sf $3$4/adc11_value /bsp/environment/$2_3vbmc
+    ln -sf $3$4/adc12_value /bsp/environment/$2_2vddr
+    ln -sf $3$4/adc13_value /bsp/environment/$2_1ddr
+    ln -sf $3$4/adc14_value /bsp/environment/$2_1vcore
   fi
   if [ "$2" == "UCD" ]; then
     mkdir -p /bsp/environment/
-    ln -s $3$4/in2_input /bsp/environment/$2_3vsen
-    ln -s $3$4/in3_input /bsp/environment/$2_1v
+    ln -sf $3$4/in2_input /bsp/environment/$2_3vsen
+    ln -sf $3$4/in3_input /bsp/environment/$2_1v
   fi
   if [ "$2" == "VcoreUCD" ]; then
     mkdir -p /bsp/environment/
-    ln -s $3$4/in2_input /bsp/environment/$2
+    ln -sf $3$4/in2_input /bsp/environment/$2
   fi
   if [ "$2" == "reset" ]; then
     mkdir -p /bsp/reset/
-    ln -s $3$4/bmc_reset_soft /bsp/reset/bmc_reset_soft
-    ln -s $3$4/cpu_reset_hard /bsp/reset/cpu_reset_hard
-    ln -s $3$4/cpu_reset_soft /bsp/reset/cpu_reset_soft
-    ln -s $3$4/system_reset_hard /bsp/reset/system_reset_hard
-    ln -s $3$4/phy_reset /bsp/reset/reset_phy
-    ln -s $3$4/bmc_uart_en /bsp/reset/bmc_uart_en
-    ln -s $3$4/uart_sel /bsp/reset/uart_sel
-    ln -s $3$4/bmc_upgrade /bsp/reset/bmc_upgrade
+    ln -sf $3$4/bmc_reset_soft /bsp/reset/bmc_reset_soft
+    ln -sf $3$4/cpu_reset_hard /bsp/reset/cpu_reset_hard
+    ln -sf $3$4/cpu_reset_soft /bsp/reset/cpu_reset_soft
+    ln -sf $3$4/system_reset_hard /bsp/reset/system_reset_hard
+    ln -sf $3$4/phy_reset /bsp/reset/reset_phy
+    ln -sf $3$4/bmc_uart_en /bsp/reset/bmc_uart_en
+    ln -sf $3$4/uart_sel /bsp/reset/uart_sel
+    ln -sf $3$4/bmc_upgrade /bsp/reset/bmc_upgrade
   fi
   if [ "$2" == "fan" ]; then
     mkdir -p /bsp/fan/
-    ln -s $3$4/tacho0_en /bsp/fan/tacho1_en
-    ln -s $3$4/tacho1_en /bsp/fan/tacho2_en
-    ln -s $3$4/tacho2_en /bsp/fan/tacho3_en
-    ln -s $3$4/tacho3_en /bsp/fan/tacho4_en
-    ln -s $3$4/tacho4_en /bsp/fan/tacho5_en
-    ln -s $3$4/tacho5_en /bsp/fan/tacho6_en
-    ln -s $3$4/tacho6_en /bsp/fan/tacho7_en
-    ln -s $3$4/tacho7_en /bsp/fan/tacho8_en
+    ln -sf $3$4/tacho0_en /bsp/fan/tacho1_en
+    ln -sf $3$4/tacho1_en /bsp/fan/tacho2_en
+    ln -sf $3$4/tacho2_en /bsp/fan/tacho3_en
+    ln -sf $3$4/tacho3_en /bsp/fan/tacho4_en
+    ln -sf $3$4/tacho4_en /bsp/fan/tacho5_en
+    ln -sf $3$4/tacho5_en /bsp/fan/tacho6_en
+    ln -sf $3$4/tacho6_en /bsp/fan/tacho7_en
+    ln -sf $3$4/tacho7_en /bsp/fan/tacho8_en
 
-    ln -s $3$4/tacho0_rpm /bsp/fan/tacho1_rpm
-    ln -s $3$4/tacho1_rpm /bsp/fan/tacho2_rpm
-    ln -s $3$4/tacho2_rpm /bsp/fan/tacho3_rpm
-    ln -s $3$4/tacho3_rpm /bsp/fan/tacho4_rpm
-    ln -s $3$4/tacho4_rpm /bsp/fan/tacho5_rpm
-    ln -s $3$4/tacho5_rpm /bsp/fan/tacho6_rpm
-    ln -s $3$4/tacho6_rpm /bsp/fan/tacho7_rpm
-    ln -s $3$4/tacho7_rpm /bsp/fan/tacho8_rpm
+    ln -sf $3$4/tacho0_rpm /bsp/fan/tacho1_rpm
+    ln -sf $3$4/tacho1_rpm /bsp/fan/tacho2_rpm
+    ln -sf $3$4/tacho2_rpm /bsp/fan/tacho3_rpm
+    ln -sf $3$4/tacho3_rpm /bsp/fan/tacho4_rpm
+    ln -sf $3$4/tacho4_rpm /bsp/fan/tacho5_rpm
+    ln -sf $3$4/tacho5_rpm /bsp/fan/tacho6_rpm
+    ln -sf $3$4/tacho6_rpm /bsp/fan/tacho7_rpm
+    ln -sf $3$4/tacho7_rpm /bsp/fan/tacho8_rpm
 
-    ln -s $3$4/pwm0_en /bsp/fan/pwm_en
-    ln -s $3$4/pwm0_falling /bsp/fan/pwm
+    ln -sf $3$4/pwm0_en /bsp/fan/pwm_en
+    ln -sf $3$4/pwm0_falling /bsp/fan/pwm
 
     for i in /bsp/fan/tacho[1-8]_en; do echo 1 > $i; done
     echo 6 > /bsp/fan/pwm
   fi
   if [ "$2" == "eeprom_psu1" ]; then
     mkdir -p /bsp/fan/
-    ln -s $3$4/eeprom /bsp/fru/psu1_eeprom
+    ln -sf $3$4/eeprom /bsp/fru/psu1_eeprom
   fi
   if [ "$2" == "eeprom_psu2" ]; then
     mkdir -p /bsp/fan/
-    ln -s $3$4/eeprom /bsp/fru/psu2_eeprom
+    ln -sf $3$4/eeprom /bsp/fru/psu2_eeprom
   fi
   if [ "$2" == "eeprom_fan1" ]; then
     mkdir -p /bsp/fru/
-    ln -s $3$4/eeprom /bsp/fru/fan1_eeprom
+    ln -sf $3$4/eeprom /bsp/fru/fan1_eeprom
   fi
   if [ "$2" == "eeprom_fan2" ]; then
     mkdir -p /bsp/fru/
-    ln -s $3$4/eeprom /bsp/fru/fan2_eeprom
+    ln -sf $3$4/eeprom /bsp/fru/fan2_eeprom
   fi
   if [ "$2" == "eeprom_fan3" ]; then
     mkdir -p /bsp/fru/
-    ln -s $3$4/eeprom /bsp/fru/fan3_eeprom
+    ln -sf $3$4/eeprom /bsp/fru/fan3_eeprom
   fi
   if [ "$2" == "eeprom_fan4" ]; then
     mkdir -p /bsp/fru/
-    ln -s $3$4/eeprom /bsp/fru/fan4_eeprom
+    ln -sf $3$4/eeprom /bsp/fru/fan4_eeprom
   fi
   if [ "$2" == "fan1_green" ]; then
     mkdir -p /bsp/leds/fan/green/1/
-    ln -s $3$4/brightness /bsp/leds/fan/green/1/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/green/1/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/green/1/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/green/1/trigger
   fi
   if [ "$2" == "fan1_red" ]; then
     mkdir -p /bsp/leds/fan/red/1/
-    ln -s $3$4/brightness /bsp/leds/fan/red/1/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/red/1/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/red/1/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/red/1/trigger
   fi
   if [ "$2" == "fan2_green" ]; then
     mkdir -p /bsp/leds/fan/green/2/
-    ln -s $3$4/brightness /bsp/leds/fan/green/2/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/green/2/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/green/2/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/green/2/trigger
   fi
   if [ "$2" == "fan2_red" ]; then
     mkdir -p /bsp/leds/fan/red/2/
-    ln -s $3$4/brightness /bsp/leds/fan/red/2/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/red/2/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/red/2/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/red/2/trigger
   fi
   if [ "$2" == "fan3_green" ]; then
     mkdir -p /bsp/leds/fan/green/3/
-    ln -s $3$4/brightness /bsp/leds/fan/green/3/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/green/3/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/green/3/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/green/3/trigger
   fi
   if [ "$2" == "fan3_red" ]; then
     mkdir -p /bsp/leds/fan/red/3/
-    ln -s $3$4/brightness /bsp/leds/fan/red/3/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/red/3/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/red/3/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/red/3/trigger
   fi
   if [ "$2" == "fan4_green" ]; then
     mkdir -p /bsp/leds/fan/green/4/
-    ln -s $3$4/brightness /bsp/leds/fan/green/4/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/green/4/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/green/4/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/green/4/trigger
   fi
   if [ "$2" == "fan4_red" ]; then
     mkdir -p /bsp/leds/fan/red/4/
-    ln -s $3$4/brightness /bsp/leds/fan/red/4/brightness
-    ln -s $3$4/trigger /bsp/leds/fan/red/4/trigger
+    ln -sf $3$4/brightness /bsp/leds/fan/red/4/brightness
+    ln -sf $3$4/trigger /bsp/leds/fan/red/4/trigger
   fi
   if [ "$2" == "status_green" ]; then
     mkdir -p /bsp/leds/status/green/
-    ln -s $3$4/brightness /bsp/leds/status/green/brightness
-    ln -s $3$4/trigger /bsp/leds/status/green/trigger
+    ln -sf $3$4/brightness /bsp/leds/status/green/brightness
+    ln -sf $3$4/trigger /bsp/leds/status/green/trigger
   fi
   if [ "$2" == "status_red" ]; then
     mkdir -p /bsp/leds/fan/red/1/
-    ln -s $3$4/brightness /bsp/leds/status/red/brightness
-    ln -s $3$4/trigger /bsp/leds/status/red/trigger
+    ln -sf $3$4/brightness /bsp/leds/status/red/brightness
+    ln -sf $3$4/trigger /bsp/leds/status/red/trigger
   fi
 elif [ "$1" == "change" ]; then
   if [ "$2" == "reset" ]; then
@@ -178,14 +178,14 @@ elif [ "$1" == "change" ]; then
     unlink /bsp/reset/bmc_uart_en
     unlink /bsp/reset/uart_sel
     unlink /bsp/reset/bmc_upgrade
-    ln -s $3$4/bmc_reset_soft /bsp/reset/bmc_reset_soft
-    ln -s $3$4/cpu_reset_hard /bsp/reset/cpu_reset_hard
-    ln -s $3$4/cpu_reset_soft /bsp/reset/cpu_reset_soft
-    ln -s $3$4/system_reset_hard /bsp/reset/system_reset_hard
-    ln -s $3$4/phy_reset /bsp/reset/reset_phy
-    ln -s $3$4/bmc_uart_en /bsp/reset/bmc_uart_en
-    ln -s $3$4/uart_sel /bsp/reset/uart_sel
-    ln -s $3$4/bmc_upgrade /bsp/reset/bmc_upgrade
+    ln -sf $3$4/bmc_reset_soft /bsp/reset/bmc_reset_soft
+    ln -sf $3$4/cpu_reset_hard /bsp/reset/cpu_reset_hard
+    ln -sf $3$4/cpu_reset_soft /bsp/reset/cpu_reset_soft
+    ln -sf $3$4/system_reset_hard /bsp/reset/system_reset_hard
+    ln -sf $3$4/phy_reset /bsp/reset/reset_phy
+    ln -sf $3$4/bmc_uart_en /bsp/reset/bmc_uart_en
+    ln -sf $3$4/uart_sel /bsp/reset/uart_sel
+    ln -sf $3$4/bmc_upgrade /bsp/reset/bmc_upgrade
   fi
 else
   if [ "$2" == "amb_current" ] || [ "$2" == "amb_switch" ]; then
@@ -263,6 +263,8 @@ else
     unlink /bsp/fan/tacho6_rpm
     unlink /bsp/fan/tacho7_rpm
     unlink /bsp/fan/tacho8_rpm
+    unlink /bsp/fan/pwm_en
+    unlink /bsp/fan/pwm
   fi
   if [ "$2" == "eeprom_psu1" ]; then
     unlink /bsp/fru/psu1_eeprom
